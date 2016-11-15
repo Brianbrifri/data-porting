0d0df921c515fbe567026d153d576b36234fee97
trokair <wandos1@yahoo.com>
2016-11-14 21:37:34 -0600

    Add SSOID to Set
    Adding only ssoids (not student ids) to the set of ids that need to be
    sent to a file

---
+++ src/SSOIDFetchUtility.java
@@ -0,0 +1,129 @@
+/*
+ *Authors: Jacob Taylor
+ */
+
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.*;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class SSOIDFetchUtility
+{
+    //set of all ssoids that does not contain any duplicates
+    private static HashSet<String> ssoidSet = new HashSet<>();
+
+    public static void main(String[] args) throws IOException
+    {
+        String excelFilePath = "C:/Users/Me/Downloads/test1.xls";
+        System.out.println ("Starting parsing!");
+        ReadInWorkbook(excelFilePath);
+
+    }
+
+    private static void ReadInWorkbook (String path)
+    {
+         /*
+         * Each workbook is split into two parts! The header which contains information about the assessment
+         * and the second part is the data area where all the information is.
+         */
+        Workbook workbook = null;
+        FileInputStream inputStream = null;
+
+        //these are indexes that specify what column each type of data is in in the data area
+        int idIndex = -1;
+
+        //flag to check if the program is in the data area
+        boolean hasEnteredData = false;
+
+        try
+        {
+            inputStream = new FileInputStream(new File(path));
+            workbook = new HSSFWorkbook(inputStream);
+        }
+        catch (IOException e)
+        {
+            System.out.println ("File not found");
+        }
+
+        Sheet firstSheet = null;
+        try
+        {
+            firstSheet = workbook.getSheetAt(0);
+        }catch (NullPointerException e)
+        {
+            System.out.println (e.toString());
+        }
+
+        Iterator<Row> iterator = firstSheet.iterator();
+
+        int columnPosition;
+
+        //work on the the cells within the document
+        while (iterator.hasNext()) {
+            Row nextRow = iterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+
+
+            columnPosition = 0;
+            //go through all the cells in each row
+            while (cellIterator.hasNext())
+            {
+                Cell cell = cellIterator.next();
+
+
+                //if the id index is -1 it means that the program has not come accross the correct info
+                if (idIndex  == -1)
+                {
+                    //this means we have not come accrossed the header for the data area yet
+                    if (cell.getStringCellValue().compareToIgnoreCase("student") != 0 && !hasEnteredData)
+                    {
+                        //therefore just break out of loop to go to next row
+                        break;
+                    }
+                    //now entering data area copy the column indexes for each type
+                    else
+                    {
+                        hasEnteredData = true;
+
+                        //copy all indexes down  by checking for keywords in header like ID or date
+                        if (cell.getStringCellValue().contains("ID"))
+                            idIndex =  columnPosition;
+                    }
+                }
+                //we are storing the data
+                else
+                {
+                    //add the id to the set if it is a ssoid which cant be parsed to an int
+                    if (columnPosition == idIndex)
+                    {
+                        try
+                        {
+                            //if this can be parsed as a float then that means its an a student id
+                            //not an ssoid
+                            Float.parseFloat(cell.toString());
+                        }
+                        //this means that the id was a ssiod so add it to the set of them that needs to be put into a file
+                        catch (NumberFormatException e)
+                        {
+                            ssoidSet.add(cell.toString());
+                        }
+                    }
+                }
+
+                columnPosition++;
+            }
+        }
+
+    }
+
+}
